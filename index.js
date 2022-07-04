b85d41178b123ebec84d903d7b313b8a00d07194
Adam Kaminski <arturkars01@gmail.com>
2022-07-04 10:59:10 +0200

    Server creation and module installs

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Entrypoint
+//imports
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.listen(process.env.PORT, ()=>{console.log(`Server is active on port: ${process.env.PORT}`)});
