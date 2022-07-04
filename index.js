92fbdae5b763e9368e399b529915e38b7ddd7aa7
BrinyLocket <jaredswanzen@gmail.com>
2022-07-04 13:48:55 +0200

    UPDATE: Working orientation, buggy motion

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 //imports
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+let server, io;
 
-app.listen(process.env.PORT, ()=>{console.log(`Server is active on port: ${process.env.PORT}`)});
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server = http.Server(app);
+server.listen(5000);
+
+io = socketio(server);
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('motion', function (data) {
+      let a = data.acceleration;
+      console.log('MOTION: ' + a.x + ' ' + a.y + ' ' + a.z );
+      // console.log('MOTION OBJECT: ' + JSON.stringify(data, null, 4) + '\n');
+    });
+  
+    socket.on('orientation', function (data) {
+      console.log('ORIENTATION: ' + data.alpha + ' ' + data.beta + ' ' + data.gamma);
+      // console.log('ORIENTATION OBJECT: ' + JSON.stringify(data, null, 4) + '\n');
+    });
+  
+});
