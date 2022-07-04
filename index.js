b088a5515f903eeb54a1864041bd62c74aa16910
BrinyLocket <jaredswanzen@gmail.com>
2022-07-04 16:36:56 +0200

    NEW: ToneJS

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const ssl = https.createServer(
 	{
 		key: fs.readFileSync(path.join(__dirname, './certs/key.pem'), 'utf-8'),
